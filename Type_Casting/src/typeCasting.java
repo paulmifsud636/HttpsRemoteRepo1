3918fd9d3838f64963b2b5a441cc8594f1469189
Paul Mifsud <plmifsud@gmail.com>
2021-06-14 18:40:44 +0200

    typeCasting ver 01

---
+++ Type_Casting/src/typeCasting.java
@@ -0,0 +1,12 @@
+
+public class typeCasting 
+{
+
+	public static void main(String[] args)
+	{
+			//Declaring String variable and converting it to integer using Integer.parseInt() 
+			String s="1234";  
+			int i=Integer.parseInt(s);  
+			System.out.println(i);  
+	}
+}
